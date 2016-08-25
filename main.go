ba6606af46400b7a6bce24d67b33b433119da381
Janne Vikstedt <janne.vikstedt@eficode.com>
2016-08-25 14:02:35 +0300

    Basic server to handle job create, getOne and getAll

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/boltdb/bolt"
+	"github.com/jvikstedt/alarmii/server"
+	"github.com/jvikstedt/alarmii/server/repository"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	db, err := bolt.Open("development.db", 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	jobRepository := repository.NewJobRepository(db)
+	server := server.NewServer(3000, jobRepository)
+	err = server.Start()
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ server/server.go
@@ -0,0 +1,65 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/jvikstedt/alarmii/domain"
+	"github.com/jvikstedt/alarmii/server/repository"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/tylerb/graceful"
+)
+
+type Server struct {
+	Port          int
+	JobRepository *repository.JobRepository
+}
+
+func NewServer(port int, jobRepository *repository.JobRepository) Server {
+	return Server{Port: port, JobRepository: jobRepository}
+}
+
+func (s Server) Start() error {
+	e := echo.New()
+	g := e.Group("/api/v1")
+	g.POST("/jobs", s.CreateJob)
+	g.GET("/jobs", s.GetAllJobs)
+	g.GET("/jobs/:id", s.FindOneJob)
+	std := standard.New(fmt.Sprintf(":%d", s.Port))
+	std.SetHandler(e)
+	err := graceful.ListenAndServe(std.Server, 5*time.Second)
+	return err
+}
+
+func (s Server) GetAllJobs(c echo.Context) error {
+	jobs, err := s.JobRepository.GetAll()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+	return c.JSON(http.StatusOK, jobs)
+}
+
+func (s Server) FindOneJob(c echo.Context) error {
+	sid := c.Param("id")
+	id, _ := strconv.Atoi(sid)
+	job, err := s.JobRepository.GetOne(id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+	return c.JSON(http.StatusOK, job)
+}
+
+func (s Server) CreateJob(c echo.Context) error {
+	job := domain.Job{}
+	if err := c.Bind(&job); err != nil {
+		return err
+	}
+	job, err := s.JobRepository.Create(job)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+	return c.JSON(http.StatusCreated, job)
+}
